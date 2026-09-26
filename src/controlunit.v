5804cf34650c4ec30e09914f477d1db237005f35
Ömer Buğra <omerbugraeminov@gmail.com>
2026-09-26 15:41:19 +0300

    bug fix

--- src/controlunit.v
+++ src/controlunit.v
@@ -5,6 +5,14 @@ module controlunit(
     );
     localparam nop = 4'b0000;
     assign jump = (opcode == 4'b0001);
-    assign wenable = (opcode == 4'b0010);
+    always @(*) begin
+        case(opcode)
+        4'b0011, 4'b0100, 4'b0101, 4'b0110,
+        4'b0111, 4'b1000, 4'b1001, 4'b1010 :wenable = 1'b1;
+        default: wenable = 1'b0;
+    endcase
+    end
+
+    
     
 endmodule
